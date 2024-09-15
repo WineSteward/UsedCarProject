2bce39a05e6fcd76fe889ff1b1d707e0dd7e3660
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-09-15 11:37:56 +0100

    model relationships

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Car extends Model
 {
     use HasFactory;
+
+    public function stand(): BelongsTo
+    {
+        return $this->belongsTo(Stand::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
